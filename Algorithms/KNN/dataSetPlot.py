bd93dfa5e5f297a826d77ff7132da41c3b1a7397
Tanishq Rawat <67191912+tanishq1710h@users.noreply.github.com>
2023-06-25 14:07:03 +0530

    Create dataSetPlot.py

---
+++ Algorithms/KNN/dataSetPlot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot
+matplotlib.pyplot.plot(2,3,'go')
+matplotlib.pyplot.plot(1,2,'go')
+matplotlib.pyplot.plot(1,4,'go')
+matplotlib.pyplot.plot(2,4,'go')
+matplotlib.pyplot.plot(1,3,'go')
+matplotlib.pyplot.plot(3,2,'go')
+matplotlib.pyplot.plot(3.5,1,'go')
+matplotlib.pyplot.plot(1,3.5,'go')
+matplotlib.pyplot.plot(0.5,1.5,'go')
+
+
+matplotlib.pyplot.plot(4,3.5,'ko')
+
+matplotlib.pyplot.plot(6,3,'ro')
+matplotlib.pyplot.plot(5,2,'ro')
+matplotlib.pyplot.plot(4,4,'ro')
+matplotlib.pyplot.plot(3.5,4.5,'ro')
+matplotlib.pyplot.plot(5,4,'ro')
+matplotlib.pyplot.plot(6,3,'ro')
+matplotlib.pyplot.plot(5,3,'ro')
+matplotlib.pyplot.show()
